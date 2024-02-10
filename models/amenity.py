97d038153500852652a9a9f6160c29d839f9589a
johnenes <johneneojo1@gmail.com>
2024-02-10 07:06:52 -0800

    pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Define the Amenity class"""
-
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
+
     """Represent an amenity
 
     Attribute:
@@ -11,4 +12,3 @@ class Amenity(BaseModel):
     """
 
     name = ""
-    
