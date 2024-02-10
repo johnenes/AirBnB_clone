58b237f94f7a21992843e8a6a08ac94a217b08e4
johnenes <johneneojo1@gmail.com>
2024-02-10 07:29:31 -0800

    pycodestyle

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ class Review(BaseModel):
     """Represent a review
 
     Attributes:
-        place_id(str): The place id 
+        place_id(str): The place id
         user_id(str): The user id
         text(str):The text of the review
 
@@ -16,4 +16,3 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
-
